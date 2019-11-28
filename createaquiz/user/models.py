a9d98a74f022a1a8a7b41ec24e28adee77b76822
ranajaydas <ranajay@outlook.com>
2019-11-28 09:57:29 +0100

    Removed image resize feature to avoid duplicate images

--- createaquiz/user/models.py
+++ createaquiz/user/models.py
@@ -1,10 +1,9 @@
 from django.conf import settings
 from django.db import models
 from django.shortcuts import reverse
-from core.utils import ImageResizeUploadToS3Model
 
 
-class Profile(ImageResizeUploadToS3Model, models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     name = models.CharField(max_length=255, default='')
     slug = models.SlugField(max_length=30, unique=True)
