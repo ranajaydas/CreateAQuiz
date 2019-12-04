0ad0dc0e820fbf256b6462998013aa696fc991bd
ranajaydas <ranajay@outlook.com>
2019-12-04 16:19:38 +0100

    Updated STATIC_ROOT

--- createaquiz/createaquiz/settings.py
+++ createaquiz/createaquiz/settings.py
@@ -149,7 +149,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Media files
