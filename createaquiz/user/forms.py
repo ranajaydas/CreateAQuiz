437526369e2eebbf6645039144e73d60b1f9b5e3
ranajaydas <ranajay@outlook.com>
2019-12-07 08:53:04 +0100

    Enforced unique emails and captcha test in registration

--- createaquiz/user/forms.py
+++ createaquiz/user/forms.py
@@ -1,3 +1,5 @@
+import random
+
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
@@ -7,11 +9,16 @@ from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
+    random_number_1 = random.randint(1, 20)
+    random_number_2 = random.randint(1, 20)
+
     email = forms.EmailField()  # Make email a mandatory field
+    captcha = forms.IntegerField(label='What is {} + {}?'.format(random_number_1, random_number_2),
+                                 widget=forms.TextInput)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'email', 'password1', 'password2', 'captcha']
 
     def clean_username(self):
         username = self.cleaned_data['username']
@@ -25,6 +32,19 @@ class UserRegisterForm(UserCreationForm):
         if username in disallowed:
             raise ValidationError("A user with that username already exists.")
         return username
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("A user with that email already exists.")
+        return email
+
+    def clean_captcha(self):
+        captcha = self.cleaned_data['captcha']
+        if captcha == self.random_number_1 + self.random_number_2:
+            return captcha
+        else:
+            raise ValidationError("That is incorrect.")
 
     def save(self, **kwargs):
         user = super().save(commit=False)
